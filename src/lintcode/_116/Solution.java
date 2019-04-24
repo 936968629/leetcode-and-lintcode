5bbb9686d11b2cfb8da8a90be2761c168115b2ab
wenjunlin <wenjunlin@100tal.com>
2019-04-24 20:34:45 +0800

    init

--- src/lintcode/_116/Solution.java
+++ src/lintcode/_116/Solution.java
@@ -4,11 +4,31 @@ public class Solution {
 
     public boolean canJump(int[] A) {
         // write your code here
+<<<<<<< HEAD
 
 
 
         return false;
 
+=======
+        int length = A.length;
+        if (length == 0){
+            return false;
+        }
+        if (A[0] == 0){
+            return false;
+        }
+        int reach = 0;
+        for (int i = 0; i <= reach ; i++) {
+            if (reach < A[i] + i){
+                reach = i + A[i];
+            }
+            if (reach >= length-1){
+                return true;
+            }
+        }
+        return false;
+>>>>>>> 90043515c2c99e1e7490b18c478cac5f3f9a7240
     }
 
 }

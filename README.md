449b48b867b353ea62fe104f61df125a5b53abb1
wenjunlin <wenjunlin@100tal.com>
2019-04-26 12:19:11 +0800

    init

--- README.md
+++
@@ -1,4 +0,0 @@
-# leetcode-and-lintcode
-(JAVA)leetcode和lintcode算法刷题大全，持续更新.......
-
-欢迎大家一起讨论学习！
--- src/leetcode/_445/ListNode.java
+++
@@ -1,8 +0,0 @@
-package com.leetcode._445;
-
-public class ListNode {
-
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}

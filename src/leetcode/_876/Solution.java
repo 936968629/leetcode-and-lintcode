d70cbd497a1f6abe07ef3eae75f22009266de52f
wenjunlin <wenjunlin@100tal.com>
2020-07-13 20:02:38 +0800

    713

--- src/leetcode/_876/Solution.java
+++ src/leetcode/_876/Solution.java
@@ -8,9 +8,15 @@ public class Solution {
         if (head == null || head.next == null) {
             return head;
         }
+        ListNode slow = head;
+        ListNode fast = head;
 
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
 
-        return null;
     }
 
 }
